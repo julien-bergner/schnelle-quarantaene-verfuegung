af7f7910a008b6e9e9799118b331ed32e4ab6799
Julien Bergner <julien@bergner.fr>
2020-04-05 15:34:20 +0200

    Fix action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => ENV['MAIL_FROM'] }
+  config.action_mailer.default_url_options = { :host => ENV['MAIL_HOST'] }
 end
